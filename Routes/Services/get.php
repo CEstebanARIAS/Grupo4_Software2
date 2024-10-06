d009f246bb5686848abe23b64d7841765a0684fd
Esteban Arias <carlos.caro199987@gmail.com>
2024-10-05 22:14:32 -0500

    prueba 2

--- Routes/Services/get.php
+++ Routes/Services/get.php
@@ -16,7 +16,9 @@ $response = new GetController();
 
 
 if(isset($_GET["linkTo"]) && isset($_GET["equalTo"]) && !isset($_GET["rel"]) && !isset($_GET["type"])){
+
     $response->getDataFilter($table, $select, $_GET["linkTo"],$_GET["equalTo"], $orderBy, $orderMode, $startAt, $endAt);
+
 }else if(isset($_GET["rel"]) && isset($_GET["type"]) && $table == "relations" && !isset($_GET["linkTo"]) && !isset($_GET["equalTo"])){
 
 	$response -> getRelData($_GET["rel"],$_GET["type"],$select,$orderBy,$orderMode,$startAt,$endAt);
@@ -26,7 +28,7 @@ Peticiones GET con filtro entre tablas relacionadas
 =============================================*/
 
 }else{   
-    
+    echo('Else de get en services');
     $response->getData($table, $select, $orderBy, $orderMode, $startAt, $endAt);
 }
 ?>
